f4a8db3aa390e46ff01d8a549deae33700eac74c
Christian-Zwinkels-CAS <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2019-09-30 22:18:44 +0200

    Update Lesson 1.py

--- Lessons/Basics/Lesson 1.py
+++ Lessons/Basics/Lesson 1.py
@@ -11,5 +11,5 @@ print("Hello World")
 print("This is an int " + str(intiger)) #Use the str argument to convert to string
 print("This is a float " + str(floating_point))
 print("This is a string " + string) #Doesnt need one since it is already a string
-print("The length of it is: " + str(len(string)))
+print("The length of it is: " + str(len(string))) #Prints the lenght of the string
 print("This is a boolean " + str(boolean))
