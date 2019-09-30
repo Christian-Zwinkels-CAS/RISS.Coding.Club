c152347bc2c4feb772d27d1c58929f51178c42c5
Christian-Zwinkels-CAS <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2019-09-30 20:37:20 +0200

    Add files via upload

---
+++ Lesson_1.py
@@ -0,0 +1,12 @@
+#Outputing to console and declaring basic variable types
+
+
+intiger = 5 #A digit values
+floating_point = 3.14159 #Can have decimal places
+string = "This is a string" #Just text
+
+print("Hello World")
+
+print("This is an int " + str(intiger)) #Use the str argument to convert to string
+print("This is a float " + str(floating_point))
+print("This is a string " + string) #Doesnt need one since it is already a string
