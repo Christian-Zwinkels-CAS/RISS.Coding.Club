4afba095b858d467af66c58aaead18d1e1bbe27b
Christian-Zwinkels-CAS <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2019-09-30 20:41:54 +0200

    Update Lesson 1.py

--- Lessons/Lesson 1.py
+++ Lessons/Lesson 1.py
@@ -4,9 +4,11 @@
 intiger = 5 #A digit values
 floating_point = 3.14159 #Can have decimal places
 string = "This is a string" #Just text
+boolean = True #This is a boolean which can only be True or False
 
 print("Hello World")
 
 print("This is an int " + str(intiger)) #Use the str argument to convert to string
 print("This is a float " + str(floating_point))
 print("This is a string " + string) #Doesnt need one since it is already a string
+print("This is a boolean " + str(boolean))
