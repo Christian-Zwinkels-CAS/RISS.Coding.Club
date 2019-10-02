fc7f7e24a3780e7a2184ce03b7db4ac3650afe63
Christian-Zwinkels-CAS <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2019-10-02 19:03:23 +0200

    Update Lesson 6.py

--- Lessons/Basics/Lesson 6.py
+++ Lessons/Basics/Lesson 6.py
@@ -3,6 +3,7 @@
 
 a = 5
 b = 44
+lis = ["Just", "as", "an", "example"]
 
 if a == 10: #If a is equal to 10 (= is used to assign variables, == is used to compare)
 	 print("a is equal to ten") #If true do this code
@@ -17,3 +18,8 @@ if b > a:
 		print("it is also bigger than 20")
 else:
 	print("b is less than a and 20")
+
+if "an" in lis: #If the string an is in the list lis
+	print(lis[2])
+else: #You dont have to put an else if you dont need to but i needed to teach quickly what pass does
+	pass #Tells python to move on 
