f86a05b3f9d86ad79f27708b776c34739f6e6608
Christian-Zwinkels-CAS <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2019-10-03 22:15:38 +0200

    Create Lesson 2.py

---
+++ Lessons/GUI Programming/Lesson 2.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+root = Tk()
+root.title("Using the grid layout")
+
+usr = Label(root, text="Username", anchor=W) #Anchor aligns the text in this case to the west (yeah I know its weird)
+passwr = Label(root, text="Password", anchor=W)
+
+but = Button(root, text="Show me my username and password")
+
+usr_e = Entry(root) #Creates an entry box with allows string input from the user
+passwr_e = Entry(root)
+
+usr.grid(row=0, column=0, sticky=W) #Places it to row 1 and column 1 aligning  it to the west
+passwr.grid(row=1, column=0, sticky=W)
+
+but.grid(row=3, column=0, columnspan=2) #Columnspan lets the object span over 2 columns
+
+usr_e.grid(row=0, column=1)
+passwr_e.grid(row=1, column=1)
+
+root.mainloop()
