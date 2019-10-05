f96de6d45f35e9e884384d970f63e8b1f59742fc
Christian-Zwinkels-CAS <55996897+Christian-Zwinkels-CAS@users.noreply.github.com>
2019-10-05 14:32:49 +0200

    Update Lesson 7.py

--- Lessons/Basics/Lesson 7.py
+++ Lessons/Basics/Lesson 7.py
@@ -1,8 +1,19 @@
 #While loops
 
 
-x = 1
+i = 1
+while i < 8: #While x is less than 8 loop through this code
+	if i == 6:
+		break #Tells python to stop the loop
+	print(i)
+	i += 1
 
-while x < 8: #While x is less than 8 loop through this code
-	print(x)
-	x += 1
+x = 0
+while x < 12:
+	x += 2
+	if x == 8:
+		print("Skips 8")
+		continue #Skips when x is 8
+	print("X: " + str(x))
+else: #When the loop is done do this code
+	print("x is now not less than 12")
